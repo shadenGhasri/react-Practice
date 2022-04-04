b21f1ba7d5dc3fb39535a41fa8d5b7e9093865f1
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-04-04 13:51:15 +0430

    add

---
+++ react-proj/src/component/product.jsx
@@ -0,0 +1,17 @@
+import {Component} from 'react';
+class Product extends Component {
+    // state = {  } 
+    render() { 
+        return (
+            <>
+                <span>Product Name</span>
+                <span>2</span>
+                <button>+</button>
+                <button>-</button>
+                <button>delete</button>
+            </>
+        );
+    }
+}
+ 
+export default Product;
--- react-proj/src/index.js
+++ react-proj/src/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Product from "./component/product";
 import  ReactDOM  from "react-dom";
 
 const Elem = <h1>hi shaden</h1>

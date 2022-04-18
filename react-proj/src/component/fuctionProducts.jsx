a55295bd404132a81dae0d62a21877b99babaa5a
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-04-18 19:37:01 +0430

    add

--- react-proj/src/component/fuctionProducts.jsx
+++ react-proj/src/component/fuctionProducts.jsx
@@ -1,31 +1,24 @@
-import Product from "./functionProduct";
-import { useState } from "react";
+import Product from './product';
+import {useState} from 'react';
 
 const Products = () => {
-    const [products , setProducts] = useState([
-        {id:1 , count: 2 , productName: "laptop"},
-        {id:2 , count: 3 , productName: "phone"}
-    ]);
-    return ( 
-        <>
-            {products.map(item=>(
-                <Product onIncrement = {handleIncrement} onDecrement = {handleDecrement} key={item.id} productName= {item.productName} count={item.count}/>
-            )
-            )}
-        </>
-     );
-     function handleIncrement(productID){
-        const newProducts = [...this.state.products]
-        const index = newProducts.findIndex(p=> p.id === productID) 
-        newProducts[index].count += 1;
-        setProducts(newProducts)
-    }
-    function handleDecrement(productID){
-        const newProducts = [...this.state.products]
-        const index = newProducts.findIndex(p=> p.id === productID) 
-        newProducts[index].count += 1;
-        setProducts(newProducts)
-    }
+  const [products, setProducts] = useState([
+    {id:1 , count: 2, productName: 'laptop'},
+    {id:2 , count: 1, productName: 'phone'},
+    {id:3 , count: 4, productName: 'airpods'},
+  ])
+  return ( 
+    <>
+      {products.map((p,index) => (
+          <Product onDelete={handleDelete} id={p.id} key={index} productName={p.productName} count={p.count} />
+        ))}
+    </>
+   );
+
+   function handleDelete(productId){
+    const newProducts = products.filter(p => p.id !== productId);
+    setProducts(newProducts);
+  }
 }
  
 export default Products;
--- react-proj/src/component/functionProduct.jsx
+++ react-proj/src/component/functionProduct.jsx
@@ -1,28 +1,39 @@
+import {useState} from 'react';
 
-import { useState } from "react";
+const Product = ({productName, count: propCount, onDelete, id }) => {
 
-const Product = ({productName ,count , id , onIncrement , onDecrement}) => {
-    // const [counter , setCounter] = useState(count)
-    return (
-        
-            <div>
-                <span className='m-2 text-info'>{productName}</span>
-                <span className='m-2 badge bg-primary'>{count}</span>
-                <button onClick={handleIncrement} className='m-2 btn btn-sm btn-success'>+</button>
-                <button onClick={handleDecrement} className='m-2 btn btn-sm btn-warning'>-</button>
-                <button className='m-2 btn btn-sm btn-danger'>delete</button>
-            </div>
-        
-    );
-    function handleIncrement(){
-        onIncrement(id)
+  const [count, setCount] = useState(propCount);
+
+  return (
+    <div>
+      <span  className='m-2 text-info'>{productName}</span>
+      <span  className='m-2 badge bg-primary'>{format()}</span>
+      <button onClick={handleIncrement} className='m-2 btn btn-sm btn-success'>+</button>
+      <button onClick={handleDecrement} className='m-2 btn btn-sm btn-warning'>-</button>
+      <button onClick={handleDelete} className='m-2 btn btn-sm btn-danger'>delete</button>
+    </div>
+  );
+
+  function handleIncrement(){
+    setCount(count + 1);
+  }
+
+  function handleDecrement(){
+    setCount(count - 1);
+
+  }
+
+  function handleDelete(){
+    onDelete(id);
+  }
+
+  function format(){
+    if(count === 0){
+      return 'zero';
+    }else{
+      return count;
     }
-
-    function handleDecrement(){
-        onDecrement(id)
-    }
-
-   
-};
+  }
+}
  
 export default Product;
